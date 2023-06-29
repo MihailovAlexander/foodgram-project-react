79561467f789cdb0220934c1d764051ab68e06b5
MihailovAlexander <1c934567@gmail.com>
2023-06-29 14:13:04 +0700

    add admin.py

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subscription, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'username',
+        'email',
+        'first_name',
+        'last_name'
+    ]
+    search_fields = ['username', 'email']
+    list_filter = ['username', 'email']
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'author']
+    search_fields = [
+        'user__username',
+        'user__email'
+        'author__username',
+        'author__email',
+    ]
+    list_filter = ['user__username', 'author__username']
+    empty_value_display = '-пусто-'
